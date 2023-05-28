9e7bf6c43c3a433daa20d5e4ca470d4a37ba7fe0
voidASP <voidasp@MacBook-Pro-voidASP.local>
2023-05-28 23:12:14 +0300

    Initial commit

---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,12 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+    @GetMapping("/index")
+    public String sayHello() {
+        return "index";
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,68 @@
+package web.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String lastName;
+    private byte age;
+    private String email;
+
+    public User(int id, String name, String lastName, byte age, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
